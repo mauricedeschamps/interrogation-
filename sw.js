2a9cdb310ee534cd48d27934984be4b5aa8645bb
mauricedeschamps <mementomorikatsu@gmail.com>
2026-05-08 20:34:02 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'japanese-interrogatives-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json'
+  // アイコンは動的にキャッシュしても良いが、オプション。必要に応じて追加
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
